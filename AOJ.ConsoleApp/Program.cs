2170811373a1bee7c35f8db769480b5ad2fde6b7
Angel <angelcdz@gmail.com>
2023-05-29 17:17:31 +0000

    init

---
+++ AOJ.ConsoleApp/Program.cs
@@ -0,0 +1,94 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Metadata;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+using System.Xml.Linq;
+
+namespace AOJ.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://orga0410328.crm.dynamics.com/";
+            var user = "admin@CRMbc508129.onmicrosoft.com";
+            var pwd = "abb7qay3PS";
+
+            using (var svc = new CrmServiceClient(
+                $@"AuthenticationType=Office365; url={url}; UserName={user}; Password={pwd};"))
+            {
+                var ids = new List<string>() { "5ca50030-cdfd-ed11-8f6d-000d3a9d67f2" };
+
+                var notes = svc.RetrieveMultiple(new FetchExpression($@"
+                <fetch version=""1.0"" output-format=""xml-platform"" mapping=""logical"" distinct=""false"">
+                    <entity name=""annotation"">
+                    <all-attributes />
+                    <filter type=""and"">
+                        <condition attribute=""mimetype"" operator=""eq"" value=""application/pdf"" />
+                    </filter>
+                    <link-entity name=""contact"" from=""contactid"" to=""objectid"" link-type=""inner"" alias=""aa"">
+                        <filter type=""or"">
+                        {ids.Select(x => $"<condition attribute=\"contactid\" operator=\"eq\" value=\"{x}\" />")}                            
+                        </filter>
+                    </link-entity>
+                    </entity>
+                </fetch>
+                "));
+
+                try
+                {
+                    // Create a Document and PdfCopy object
+                    Document mergedDocument = new Document();
+                    MemoryStream outputStream = new MemoryStream();
+                    PdfWriter writer = PdfWriter.GetInstance(mergedDocument, outputStream);
+
+                    mergedDocument.Open();
+
+                    foreach (var note in notes.Entities)
+                    {
+                        byte[] pdfBytes = Convert.FromBase64String(note["documentbody"].ToString());
+                        using (var pdfStream = new MemoryStream(pdfBytes))
+                        {
+                            var pdfReader = new PdfReader(pdfStream);
+
+                            for (int i = 1; i <= pdfReader.NumberOfPages; i++)
+                            {
+                                var page = writer.GetImportedPage(pdfReader, i);
+                                mergedDocument.Add(Image.GetInstance(page));
+                            }
+                        }
+                    }
+
+                    mergedDocument.Close();
+
+                    // Update the target entity with the merged PDF
+                    byte[] mergedContent = outputStream.ToArray();
+
+                    //Console.WriteLine(Convert.ToBase64String(mergedContent));
+
+                    using (FileStream fileStream = new FileStream("C:\\Users\\angelojea\\Desktop\\output\\Resume.pdf", FileMode.OpenOrCreate))
+                    {
+                        byte[] bytes = outputStream.ToArray();
+                        fileStream.Write(bytes, 0, bytes.Length);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException(ex.Message, ex);
+                }
+            }
+        }
+    }
+}
---
+++ AOJ.Plugins/PDFGenerator.cs
@@ -0,0 +1,98 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using Newtonsoft.Json;
+using System;
+using System.Activities;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AOJ.Plugins
+{
+    public class PDFGenerator : CodeActivity
+    {
+        [Input("ContactIds")]
+        public InArgument<string> ContactIds { get; set; }
+
+        [Output("PDF")]
+        public OutArgument<string> PDF { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
+            ITracingService log = context.GetExtension<ITracingService>();
+            IOrganizationService svc = context.GetExtension<IOrganizationServiceFactory>().CreateOrganizationService(workflowContext.UserId);
+
+            log.Trace("Step 1");
+            var ids = ContactIds.Get(context).Split(',');
+            log.Trace("Step 2");
+
+            var notes = svc.RetrieveMultiple(new FetchExpression($@"
+                <fetch version=""1.0"" output-format=""xml-platform"" mapping=""logical"" distinct=""false"">
+                    <entity name=""annotation"">
+                    <all-attributes />
+                    <filter type=""and"">
+                        <condition attribute=""mimetype"" operator=""eq"" value=""application/pdf"" />
+                    </filter>
+                    <link-entity name=""contact"" from=""contactid"" to=""objectid"" link-type=""inner"" alias=""aa"">
+                        <filter type=""or"">
+                        {ids.Select(x => $"<condition attribute=\"contactid\" operator=\"eq\" value=\"{x}\" />")}                            
+                        </filter>
+                    </link-entity>
+                    </entity>
+                </fetch>
+                "));
+            log.Trace("Step 3");
+
+            try
+            {
+                // Create a MemoryStream to hold the merged PDF
+                using (MemoryStream mergedStream = new MemoryStream())
+                {
+                    log.Trace("Step 4");
+                    // Create a Document and PdfCopy object
+                    Document mergedDocument = new Document();
+
+                    log.Trace("Step 5");
+                    PdfWriter writer = PdfWriter.GetInstance(mergedDocument, mergedStream);
+                    mergedDocument.Open();
+
+                    log.Trace("Step 6");
+                    foreach (var note in notes.Entities)
+                    {
+                        byte[] pdfBytes = Convert.FromBase64String(note["documentbody"].ToString());
+                        using (var pdfStream = new MemoryStream(pdfBytes))
+                        {
+                            var pdfReader = new PdfReader(pdfStream);
+
+                            for (int i = 1; i <= pdfReader.NumberOfPages; i++)
+                            {
+                                var page = writer.GetImportedPage(pdfReader, i);
+                                mergedDocument.Add(Image.GetInstance(page));
+                            }
+                        }
+                    }
+                    log.Trace("Step 9");
+                    mergedDocument.Close();
+
+                    // Update the target entity with the merged PDF
+                    byte[] mergedContent = mergedStream.ToArray();
+
+                    log.Trace("Step 10");
+                    PDF.Set(context, Convert.ToBase64String(mergedContent));
+                    log.Trace("Step 11");
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Trace(ex.Message);
+                log.Trace(ex.StackTrace);
+                throw new InvalidPluginExecutionException(ex.Message, ex);
+            }
+        }
+    }
+}
